488a7b8e93c41249180894a4932b381e30ddb25a
Alanascimento222 <alanascimento222@hotmail.com>
2025-08-25 21:00:16 -0300

    update 1.5

--- Script/script.js
+++ Script/script.js
@@ -1,8 +1,11 @@
 //variaveis para tamanho de senha
-const span = document.getElementById('senhaOutdoor')
+
+
+const span = document.querySelector('#senhaOutdoor')
 const button = document.getElementById('buttonSenha')
-let tamanho = 10
+let tamanho = null
 let complexidade = 1
+let typeAnimation = false
 const chars = '!@#$%^&*()-_+[]{}>\?/'
 const numeros = '0123456789'
 const letras = 'abcdefghijklmnopqrstuvwxyz'
@@ -13,27 +16,50 @@ const letrasArray = letras.split("")
 const letrasMaisArray = letrasMaiscula.split("")
 
 
+//botoes sendo ativados
+const botoes = document.querySelectorAll("#button button");
+botoes.forEach(botao => {
+    botao.addEventListener("click", () => {
+        tamanho = botao.textContent;
+        document.documentElement.style.setProperty('--leght', `${botao.textContent}`);
+        console.log("Último botão clicado:", ultimoBotaoClicado);
+    });
+});
+
 button.addEventListener('click', () => {
-            let senha = ""
+    let senha = ""
     for(let i = 0; i < tamanho; i++){
         let seleção = Math.floor(Math.random() * 4)
-                switch(seleção){
+        switch(seleção){
             case 0:
                 senha += charsArray[Math.floor(Math.random() * charsArray.length)]
                 break
-            case 1:
-                senha += letrasArray[Math.floor(Math.random() * letrasArray.length)]
-                break
-            case 2:
-                senha += letrasMaisArray[Math.floor(Math.random() * letrasMaisArray.length)]
-                break
-            case 3:
-                senha += numerosArray[Math.floor(Math.random() * numerosArray.length)]
-                break
-        }
-        console.log(seleção)
-        console.log(senha)
-
-    }
-    span.textContent = senha
-})
+                case 1:
+                    senha += letrasArray[Math.floor(Math.random() * letrasArray.length)]
+                    break
+                    case 2:
+                        senha += letrasMaisArray[Math.floor(Math.random() * letrasMaisArray.length)]
+                        break
+                        case 3:
+                            senha += numerosArray[Math.floor(Math.random() * numerosArray.length)]
+                            break
+                        }
+                        console.log(seleção)
+                        console.log(senha)
+                        
+                    }
+                    
+                    span.classList.remove('waitAnimation')
+                    if(typeAnimation = false){
+                        span.classList.add('typewriter')
+                        typeAnimation = true
+                    } else {
+                        typeAnimation = false
+                        span.classList.remove('typewriter');
+                        void span.offsetWidth
+                        span.classList.add('typewriter')
+                    }
+                    
+                    
+                    span.textContent = senha
+                })
